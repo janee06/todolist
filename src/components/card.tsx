46dc890c73295ca8b2cf325efc81c97b92bcedb4
janee06 <s2022BockovaJana@skolabaltaci.cz>
2024-02-08 13:25:00 +0100

    hihi

---
+++ src/components/card.tsx
@@ -0,0 +1,10 @@
+export default function Card() {
+    return (
+        <main className="bg-blue-300">
+            <div className={"text-3xl text-red-800"}>
+                <h1>Todo app č1</h1>
+            </div>
+
+        </main>
+    );
+}
