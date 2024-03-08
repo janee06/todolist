edd37546f424c84157e0d5b89678571728a30269
janee06 <s2022BockovaJana@skolabaltaci.cz>
2024-03-08 13:24:15 +0100

    hihi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,15 @@
+import {Input} from "@nextui-org/react";
 
-export default function Home() {
-  return (
-      <div>
-      <NavBar/>
-    <div className="flex flex-row py-4 space-x-6 ">
-        <Card title={"Todo"} description={"Description"} checked={true}/>
-        <Card title={"Todo"} description={"Description"} checked={true}/>
-        <Card title={"Todo"} description={"Description"} checked={true}/>
+export default function Home(){
+    return (<div>
+            <h2>Create your own Todo :)</h2>
+        <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+            <Input isRequired type="email" label="Title" placeholder="Enter your title for todo"/>
+            <Input type="email" label="Content"  placeholder="Enter your todo"/>
+            <Input type="email" label="autorID" placeholder="Enter your email" />
+        </div>
     </div>
-</div>
-  );
-}
-class TodoItemProps{
-    title:string;
-    description: string;
-    checked: Boolean;
-
-}
-function NavBar(){
-    return <nav className="flex justify-between bg-cyan-500">
-        <label className="hover:text-orange-400">Stránka všechny Todos </label>
-        <label className="hover:text-orange-400">Přidej Todos</label>
-    </nav>
+    );
 }
 
-
- function Card({title, description, checked}:TodoItemProps){
-    return <div className="flex flex-col items-center justify-start bg-slate-300 w-1/6 h-60 px-4 hover:bg-slate-600">
-        <h2>{title}</h2>
-        <label className="text-xs">{description}</label>
-    </div>
-}
 
