82028e8ba309ccd6df9af3b61e881e88ddb4ccde
Jana <s2022BockovaJana@skolabaltaci.cz>
2024-03-09 16:06:39 +0100

    hihi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import {Input} from "@nextui-org/react";
 
 export default function Home(){
-    return (<div>
-            <h2>Create your own Todo :)</h2>
-        <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+    return (<div className="m-5">
+            <h2 className="text-3xl font-bold">Create your own Todo :)</h2>
+        <div className="flex w-full flex-wrap md:flex-nowrap gap-4 mt-5 text-xl font-semibold">
             <Input isRequired type="email" label="Title" placeholder="Enter your title for todo"/>
             <Input type="email" label="Content"  placeholder="Enter your todo"/>
-            <Input type="email" label="autorID" placeholder="Enter your email" />
+            <Input type="email" label="AutorID" placeholder="Enter your email" />
         </div>
     </div>
     );
