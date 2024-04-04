67163d6f45dda84a401ceb9d591a461c574c9a45
janee06 <s2022BockovaJana@skolabaltaci.cz>
2024-04-04 13:23:53 +0200

    hihi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import Todo from "../components/todo";
 import {prisma} from "./db";
+import {User} from "@nextui-org/react";
+import {Checkbox} from "@nextui-org/react";
+import {Divider} from "@nextui-org/react";
 
 export type TodoType = {
     id: string;
@@ -19,20 +22,24 @@ export default async function Home() {
 
     return (
         <>
-            <header className="flex justify-between items-center pb-5">
-                <h1 className="text-2xl underline underline-offset-4 ">Todos</h1>
+              <div className="h-10 w-10 ">
+                  <User/>
+              </div>
+
+            <header className="flex justify-between items-center pb-5 ml-10">
+                <h1 className="flex w-full flex-wrap md:flex-nowrap gap-4 mt-5 mb-5 text-3xl text-indigo-700 font-semibold grid flex items-center justify-center">♥ Todos ♥</h1>
                 <Link
                     href="/newitem"
-                    className="border border-slate-300 text-slate-300 rounded px-2 py-1 hover:bg-zinc-700 focus-within:bg-zinc-700 outline-none"
+                    className="border border-indigo-500 text-indigo-700 rounded px-2 py-1 hover:bg-indigo-300 focus-within:bg-zinc-700 outline-none mr-5"
                 >
                     New
                 </Link>
             </header>
 
 
-            <ul className="pl-4">
+            <ul className="flex w-full flex-wrap md:flex-nowrap gap-2  mb-5 text grid flex items-center justify-center ">
                 {
-                    todos.map((todo: TodoType)=><div>{todo.id}{todo.title}</div>)
+                     todos.map((todo: TodoType)=> <Checkbox defaultSelected className="flex flex-col-start"> <div> {todo.id} {todo.title} <Divider/>  </div> </Checkbox>)
                 }
             </ul>
         </>
