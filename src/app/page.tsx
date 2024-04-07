be961a6f7ef555b018e45277917c93043920c4d5
Jana <s2022BockovaJana@skolabaltaci.cz>
2024-04-07 19:31:05 +0200

    hihi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ import {prisma} from "./db";
 import {User} from "@nextui-org/react";
 import {Checkbox} from "@nextui-org/react";
 import {Divider} from "@nextui-org/react";
+import {ScrollShadow} from "@nextui-org/react";
+
+
+
 
 export type TodoType = {
     id: string;
@@ -22,26 +26,37 @@ export default async function Home() {
 
     return (
         <>
-              <div className="h-10 w-10 ">
-                  <User/>
-              </div>
+            <ScrollShadow>
+            <div className="flex flex-col mt-2  ">
 
-            <header className="flex justify-between items-center pb-5 ml-10">
-                <h1 className="flex w-full flex-wrap md:flex-nowrap gap-4 mt-5 mb-5 text-3xl text-indigo-700 font-semibold grid flex items-center justify-center">♥ Todos ♥</h1>
+
+            <header className="flex justify-between items-center pb-5 ml-5">
+                <div className="h-10 w-10 ">
+                    <User className="rounded-full outline-indigo-700"/>
+                </div>
+                <h1 className="flex w-full flex-wrap md:flex-nowrap gap-4 mt-5 mb-5 text-4xl text-indigo-700 font-semibold grid flex items-center justify-center">♥ Todos ♥</h1>
                 <Link
                     href="/newitem"
-                    className="border border-indigo-500 text-indigo-700 rounded px-2 py-1 hover:bg-indigo-300 focus-within:bg-zinc-700 outline-none mr-5"
+                    className="border border-indigo-500 text-indigo-700 rounded text-xl px-2 py-1 hover:bg-indigo-300 focus-within:bg-zinc-700 outline-none mr-5"
                 >
                     New
                 </Link>
             </header>
 
+            <ul className="flex w-full flex-wrap md:flex-nowrap gap-2 text-lg mb-5 text grid flex items-center justify-center ">
 
-            <ul className="flex w-full flex-wrap md:flex-nowrap gap-2  mb-5 text grid flex items-center justify-center ">
                 {
-                     todos.map((todo: TodoType)=> <Checkbox defaultSelected className="flex flex-col-start"> <div> {todo.id} {todo.title} <Divider/>  </div> </Checkbox>)
+
+
+
+                    // eslint-disable-next-line react/jsx-key
+                    todos.map((todo: TodoType)=>  <div> {todo.id} {todo.title}  <Divider/> <Checkbox className=" flex flex-col-start"></Checkbox> </div>  )
                 }
+
             </ul>
+
+            </div>
+        </ScrollShadow>
         </>
     );
 }
